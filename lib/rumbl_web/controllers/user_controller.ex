0ceb41a4ef4b28db48d08a2e4efbe4baab9de8f7
Fábio Ueno <fabio.t.ueno@gmail.com>
2023-01-27 18:49:56 -0300

    feat(users): Add functionality to create new users
    - Create a web form to input data.
    - Use Ecto and Changesets to validate and persist data.

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -14,4 +14,22 @@ defmodule RumblWeb.UserController do
 
     render(conn, "show.html", user: user)
   end
+
+  def new(conn, _params) do
+    changeset = Accounts.change_user(%Accounts.User{})
+
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    case Accounts.create_user(user_params) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "#{user.name} created!")
+        |> redirect(to: Routes.user_path(conn, :index))
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
 end
