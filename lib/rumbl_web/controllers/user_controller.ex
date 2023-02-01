c968072bfad26f6c9cb291fc7659839389f9c562
Fábio Ueno <fabio.t.ueno@gmail.com>
2023-02-01 06:33:14 -0300

    feat(users): Register users with password

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -16,13 +16,13 @@ defmodule RumblWeb.UserController do
   end
 
   def new(conn, _params) do
-    changeset = Accounts.change_user(%Accounts.User{})
+    changeset = Accounts.change_registration(%Accounts.User{}, %{})
 
     render(conn, "new.html", changeset: changeset)
   end
 
   def create(conn, %{"user" => user_params}) do
-    case Accounts.create_user(user_params) do
+    case Accounts.register_user(user_params) do
       {:ok, user} ->
         conn
         |> put_flash(:info, "#{user.name} created!")
