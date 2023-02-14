b122bf579f3e03de5bd384c48a23cb455b865a3f
Fábio Ueno <fabio.t.ueno@gmail.com>
2023-02-14 08:50:57 -0300

    refactor(auth): Isolate authentication from UserController

--- lib/rumbl_web.ex
+++ lib/rumbl_web.ex
@@ -23,6 +23,9 @@ defmodule RumblWeb do
 
       import Plug.Conn
       import RumblWeb.Gettext
+
+      import RumblWeb.Auth, only: [authenticate_user: 2]
+
       alias RumblWeb.Router.Helpers, as: Routes
     end
   end
@@ -74,6 +77,8 @@ defmodule RumblWeb do
       import Plug.Conn
       import Phoenix.Controller
       import Phoenix.LiveView.Router
+
+      import RumblWeb.Auth, only: [authenticate_user: 2]
     end
   end
 
